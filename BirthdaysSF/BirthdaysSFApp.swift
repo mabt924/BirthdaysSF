59b023b5c4127a53988a829f9ca9836e81d62c02
Michelle Abt <mabt924@gmail.com>
2025-08-08 12:50:20 -0700

    Initial Commit

---
+++ BirthdaysSF/BirthdaysSFApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysSFApp.swift
+//  BirthdaysSF
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysSFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
